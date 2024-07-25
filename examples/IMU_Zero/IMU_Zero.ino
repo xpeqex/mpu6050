b49927110420649428348a7d9690701209ed2290
xpeqex <carlosalat01@gmail.com>
2024-07-25 15:34:13 -0600

    feat (docs): Rearange the sketch comments

--- examples/IMU_Zero/IMU_Zero.ino
+++ examples/IMU_Zero/IMU_Zero.ino
@@ -1,71 +1,36 @@
-// MPU6050 offset-finder, based on Jeff Rowberg's MPU6050_RAW
-// 2016-10-19 by Robert R. Fenichel (bob@fenichel.net)
+/*
+MPU6050 Zero Example
 
-// I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class
-// 10/7/2011 by Jeff Rowberg <jeff@rowberg.net>
-// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
-//
-// Changelog:
-//      2019-07-11 - added PID offset generation at begninning Generates first offsets 
-//                 - in @ 6 seconds and completes with 4 more sets @ 10 seconds
-//                 - then continues with origional 2016 calibration code.
-//      2016-11-25 - added delays to reduce sampling rate to ~200 Hz
-//                   added temporizing printing during long computations
-//      2016-10-25 - requires inequality (Low < Target, High > Target) during expansion
-//                   dynamic speed change when closing in
-//      2016-10-22 - cosmetic changes
-//      2016-10-19 - initial release of IMU_Zero
-//      2013-05-08 - added multiple output formats
-//                 - added seamless Fastwire support
-//      2011-10-07 - initial release of MPU6050_RAW
+This code finds the MPU6050 offsets (based on Jeff Rowberg's MPU6050_RAW)
 
-/* ============================================
-I2Cdev device library code is placed under the MIT license
-Copyright (c) 2011 Jeff Rowberg
+Features on this code:
+  - PID ofset generation in about 6 secs and completes with 4 more sets in about 10 secs.
+  - Delays to redice smapling rate to ~200Hz.
+  - Multiple output formats.
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Considerations, the module:
+  - Is an ideal memeber of its tribe.
+  - Is properly warmed up.
+  - Is resting in a neutral position.
+  - Is in a location where the pull of gravity is exactly 1g.
+  - Has been loaded with the best possible offsets.
+  - When perfectly calibrated, it will output zero values for all acceleration 
+    and displacement measurements except for Z-axis acceleration, which will be 
+    16384 (or 2^14). While your specific device might not achieve these exact values, 
+    proper calibration should bring your baseline outputs very close to these targets.
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+Instructions for a better result:
+  1.  Put the MPU6050 on a flat and horizontal surface, and leave it operating for 
+    5-10 minutes so its temperature gets stabilized.
+  2. Run this program.  A "----- done -----" line will indicate that it has done its best.
+    With the current accuracy-related constants (NFast = 1000, NSlow = 10000), it will take 
+    a few minutes to get there.
+  3. Along the way, it will generate a dozen or so lines of output, showing that for each 
+    of the 6 desired offsets. First, trying to find two estimates, one too low and one too 
+    high, and then, closing in until the bracket cannot be made smaller.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-  If an MPU6050 
-      * is an ideal member of its tribe, 
-      * is properly warmed up, 
-      * is at rest in a neutral position, 
-      * is in a location where the pull of gravity is exactly 1g, and 
-      * has been loaded with the best possible offsets, 
-then it will report 0 for all accelerations and displacements, except for 
-Z acceleration, for which it will report 16384 (that is, 2^14).  Your device 
-probably won't do quite this well, but good offsets will all get the baseline 
-outputs close to these target values.
-
-  Put the MPU6050 on a flat and horizontal surface, and leave it operating for 
-5-10 minutes so its temperature gets stabilized.
-
-  Run this program.  A "----- done -----" line will indicate that it has done its best.
-With the current accuracy-related constants (NFast = 1000, NSlow = 10000), it will take 
-a few minutes to get there.
-
-  Along the way, it will generate a dozen or so lines of output, showing that for each 
-of the 6 desired offsets, it is 
-      * first, trying to find two estimates, one too low and one too high, and
-      * then, closing in until the bracket can't be made smaller.
-
-  The line just above the "done" line will look something like
-    [567,567] --> [-1,2]  [-2223,-2223] --> [0,1] [1131,1132] --> [16374,16404] [155,156] --> [-1,1]  [-25,-24] --> [0,3] [5,6] --> [0,4]
+The line just above the "done" line will look something like
+[567,567] --> [-1,2]  [-2223,-2223] --> [0,1] [1131,1132] --> [16374,16404] [155,156] --> [-1,1]  [-25,-24] --> [0,3] [5,6] --> [0,4]
 As will have been shown in interspersed header lines, the six groups making up this
 line describe the optimum offsets for the X acceleration, Y acceleration, Z acceleration,
 X gyro, Y gyro, and Z gyro, respectively.  In the sample shown just above, the trial showed
@@ -73,7 +38,6 @@ that +567 was the best offset for the X acceleration, -2223 was best for Y accel
 and so on.
 
   The need for the delay between readings (usDelay) was brought to my attention by Nikolaus Doppelhammer.
-===============================================
 */
 
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
@@ -356,3 +320,28 @@ void setup()
 void loop()
   {
   } // loop
+
+
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+Copyright (c) 2011 Jeff Rowberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
